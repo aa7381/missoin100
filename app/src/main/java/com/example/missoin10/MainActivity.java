3a2d5f6c01556df033219e732dc7dc6e12e78415
aa7381 <aa7381@bs.amalnet.k12.il>
2024-02-29 14:58:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/missoin10/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.missoin10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int mone=0;
+    int mone2=0;
+    ToggleButton tB;
+    Switch switch1;
+    Button btn;
+    ImageView iV;
+    int change;
+    int[] id= new int[4];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tB = (ToggleButton) findViewById(R.id.tB);
+        switch1 = (Switch) findViewById(R.id.switch1);
+        btn = findViewById(R.id.btn);
+        iV =  findViewById(R.id.iV);
+        id[0]=R.drawable.banana;
+        id[1]=R.drawable.correct;
+        id[2]=R.drawable.random;
+        id[3]=R.drawable.dog;
+
+    }
+    public void clicked2(View v){
+        if(tB.isChecked()){
+            mone = 1;
+        }else {
+            mone = 0;
+        }
+
+    }
+    public void clicked3(View v){
+        if(switch1.isChecked()){
+            mone2 = 1;
+        }else {
+            mone2 = 0;
+        }
+
+    }
+    public void onClick(View view)
+    {
+        if(switch1.isChecked() && tB.isChecked())
+        {
+            iV.setImageResource(id[0]);
+        } else if (switch1.isChecked() && !tB.isChecked()) {
+            iV.setImageResource(id[1]);
+
+        } else if (!switch1.isChecked() && tB.isChecked()) {
+            iV.setImageResource(id[2]);
+
+        } else if (!switch1.isChecked() && !tB.isChecked()) {
+            iV.setImageResource(id[3]);
+        }
+
+
+    }
+}
